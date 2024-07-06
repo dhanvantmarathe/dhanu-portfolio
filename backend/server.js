917790dd3e4b08a8388d2a4ccd34ba6473a0c628
Dhanvant_Marathe <116859774+dhanvantmarathe@users.noreply.github.com>
2024-07-06 20:42:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://portfolio-frontend-rust-three.vercel.app', // Your frontend URL
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true // Enable this if you need to include cookies in the requests
+}));
 
 // Connect to MongoDB
 mongoose.connect(mongoURI, {
